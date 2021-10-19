f809c4febb62afd93c6541c260e4d19716ce8504
Caner Erden <cerden@sakarya.edu.tr>
2021-10-19 18:35:49 +0300

    deleted

--- matplotlib/sin_cos_grafigi.py
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-get_ipython().magic('save sin_cos_grafigi 115 # 115. çalıştırılan hücredeki kodları kaydetmek için')
-plt.plot(x, np.sin(x))  
-plt.plot(x, np.cos(x))  
-plt.plot(x, x**2) 
-plt.legend(['sinüs', 'cosinüs', 'kare'], loc = 'upper left')
